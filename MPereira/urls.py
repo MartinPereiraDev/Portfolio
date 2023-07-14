1c6bdc8a68b1227c0e374788aa828855ab28f519
Martin Pereira <tin900@hotmail.com>
2023-07-14 15:50:08 -0300

    Arreglando entorno virtual

--- MPereira/urls.py
+++ MPereira/urls.py
@@ -10,8 +10,8 @@ from django.urls import path, include
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-     path('', include('home.urls'))
+   # path('admin/', admin.site.urls),
+   #  path('', include('home.urls'))
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
